53b847647ce9c9ef257981e8cc5c688210874ba7
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-29 15:26:19 +0300

    components

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,11 +1,22 @@
-
+import "./tailwind.css";
+import Button from "./Components/Button";
+import Tab from "./Components/Tab";
 
 function App() {
- 
-
   return (
     <>
-     
+      <div style={{ padding: 10 }}>
+        <Button variant="success" text="success Buton Örneği"></Button>
+        <Button variant="danger" text="danger Buton Örneği"></Button>
+        <Button variant="warning" text="warning Buton Örneği"></Button>
+        <Button variant="info" text="info Buton Örneği"></Button>
+
+        <Tab>
+          <Tab.Panel title="Profil">1.tab</Tab.Panel>
+          <Tab.Panel title="Hakkında">2.tab</Tab.Panel>
+          <Tab.Panel title="Ayarlar"> 3.tab</Tab.Panel>
+        </Tab>
+      </div>
     </>
   );
 }
---
+++ react_project/src/Components/Button.js
@@ -0,0 +1,21 @@
+import classNames from "classnames";
+
+function Button({ text, variant = "default" }) {
+  return (
+    <>
+      <button
+        className={classNames({
+          "p-4 h-10 flex items-center rounded": true,
+          "bg-gray-100" :variant ==='default',
+          "bg-green-600 text-white" :variant ==='success',
+          "bg-red-600 text-white" :variant ==='danger',
+          "bg-yellow-600 text-white" :variant ==='info',
+          "bg-yellow-200 text-white" :variant ==='warning',
+        })}
+      >
+        {text}
+      </button>
+    </>
+  );
+}
+export default Button;
